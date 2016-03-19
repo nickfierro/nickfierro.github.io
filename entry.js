bd2ceeca3dd47e93982ea9d5066becd89e77738b
nickfierro <nickfierro@gmail.com>
2016-03-19 13:44:55 -0400

    removed whole bootstrap folder

--- entry.js
+++ entry.js
@@ -207,5 +207,16 @@ function getBlock(){
   console.log('blockNumber', blockNumber);
   display.appendText(blockNumber)
 };
+function watchBalance() {
+    var coinbase = web3.eth.coinbase;
+    var originalBalance = web3.eth.getBalance(coinbase).toNumber();
+    document.getElementById('coinbase').innerText = 'coinbase: ' + coinbase;
+    document.getElementById('original').innerText = ' original balance: ' + originalBalance + '    watching...';
+    web3.eth.filter('latest').watch(function() {
+        var currentBalance = web3.eth.getBalance(coinbase).toNumber();
+        document.getElementById("current").innerText = 'current: ' + currentBalance;
+        document.getElementById("diff").innerText = 'diff:    ' + (currentBalance - originalBalance);
+    });
+};
 
 console.log('web3', web3);
