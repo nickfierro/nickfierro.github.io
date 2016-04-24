5751e003b3d354c3b2f73b25b4d764aafa6e236a
nickfierro <nickfierro@gmail.com>
2016-04-24 01:59:58 -0400

    ip test

--- js/web3instance.js
+++ js/web3instance.js
@@ -1,5 +1,5 @@
 var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+web3.setProvider(new web3.providers.HttpProvider('http://192.168.1.172:8545'));
 
 console.log('web3', web3);
 
